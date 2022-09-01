236d28f0a5b78c7bfb3dbf2fcbd30283fc4aff47
Confinyh <ic_irvin_rj@hotmail.com>
2022-09-01 13:55:54 -0500

    Refactor of TriangularNumber to be a class. Now can be used the same way of PrimeNumbers

--- test/components/TriangularNumber.test.ts
+++ test/components/TriangularNumber.test.ts
@@ -2,16 +2,19 @@ import { TriangularNumber } from '../../src/components/TriangularNumber';
 
 describe('Testing TriangularNumber function', () => {
 
+    const Triangular = new TriangularNumber();
+
     test('Validate the first ten triangular numbers', () => {
         const expected = [1, 3, 6, 10, 15, 21, 28, 36, 45, 55];
 
         for (let i = 1; i <= 10; i++) {
-            expect(TriangularNumber(i)).toBe(expected[i - 1]);
+            expect(Triangular.get_nth(i)).toBe(expected[i - 1]);
         }
     })
 
     test('If n <= 0 or not is a natural number should raise exception', () => {
-        expect(() => TriangularNumber(0)).toThrowError('n must be greater than 0');
-        expect(() => TriangularNumber(5.5)).toThrowError('n must be a natural number');
+        expect(() => Triangular.get_nth(1.5)).toThrowError('n must be a natural number greater than zero');
+        expect(() => Triangular.get_nth(0)).toThrowError('n must be a natural number greater than zero');
+        expect(() => Triangular.get_nth(-1)).toThrowError('n must be a natural number greater than zero');
     })
 })
